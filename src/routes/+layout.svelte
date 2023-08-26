a69450b8449069cc6f39fbd72cc4a863739fb5ac
mathyled <mathiasimagine@gmail.com>
2023-08-26 19:17:43 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/profile">Profile</a>
+        </li>
+        <li>
+            <a href="/login">Login</a>
+        </li>
+        <li>
+            <a href="/#">Logout</a>
+        </li>
+    </ul>
+</nav>
+<slot />
